14f1d61aae75e3374f91cec6392e89bddc14b89f
Guillaume Aubert <aubertg@cpan.org>
2013-06-01 22:17:53 -0400

    Released version 2.2.1.

--- lib/Audit/DBI/TT2.pm
+++ lib/Audit/DBI/TT2.pm
@@ -18,11 +18,11 @@ Audit::DBI::TT2 - A Template Toolkit plugin to display audit events recorded by 
 
 =head1 VERSION
 
-Version 2.2.0
+Version 2.2.1
 
 =cut
 
-our $VERSION = '2.2.0';
+our $VERSION = '2.2.1';
 
 
 =head1 SYNOPSIS
